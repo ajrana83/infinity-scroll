ea13c654d03583270e36dead773a7dd556e6f663
AJ <ajay.rana83@gmail.com>
2021-05-25 07:47:17 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Testing");
+// Unspalsh API
+const count = 10;
+const apikey='tEpHjllguJczyU0yv6wCUrYWIsE8RXpKBH45Kuk8fms';
+const apiURL = `https://api.unsplash.com/photos/random?client_id=${apikey}&count=${count}`;
+
+// get photo's from unsplashed API
+async function getPhotos(){
+try {
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    console.log(data);
+    
+} catch (error) {
+    console.log("Error")
+}
+}
+
+getPhotos();
